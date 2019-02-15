79f12d793d1377ceade62233d3f9e56c5bb17ea5
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2019-02-15 14:47:19 -0800

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -7,7 +7,12 @@ Input(std::string userInput){
 }
 
 bool Input::execute(){//Here we parse the string and make a tree out of objects
-
+	char *token = strtok(str," ");
+    	while (token)
+    	{
+        	
+        	token = strtok(NULL,delim);
+	}
 
 }
 
