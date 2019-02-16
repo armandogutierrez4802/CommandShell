ff3bf481e3480f20f769245c982e95e06259e5ee
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2019-02-15 16:43:47 -0800

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -7,11 +7,21 @@ Input(std::string userInput){
 }
 
 bool Input::execute(){//Here we parse the string and make a tree out of objects
-	std::string newUserString;
+	std::string newUserString = "";
 	//this->userInput += "\0"
 	
 	for(int i = 0; i < this->userInput.size(); i++){//Handle the comments from user input
-		newUserString += 
+		if(this->userInput[i] == '#'){
+			break;
+		} else{
+			if(this->userInput[i+1] == '' && (i+1) != this->userInput.size()){
+				
+			}
+			newUserString += this->userInput[i]; 
+		}	
+	}
+	
+	
 	
 	
 	char *token = strtok(str," ");
