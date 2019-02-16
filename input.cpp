ff94d70f135a844773d7dde41cabe9648002db8d
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2019-02-15 19:30:57 -0800

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -7,26 +7,38 @@ Input(std::string userInput){
 }
 
 bool Input::execute(){//Here we parse the string and make a tree out of objects
-	std::string newUserString = "";
-	//this->userInput += "\0"
+	std::string newUserInput = "";
+	//this->userInput += "\0" //Do we need this or does every string end in /0 ??
 	
-	for(int i = 0; i < this->userInput.size(); i++){//Handle the comments from user input
+	for(int i = 0; i < this->userInput.size(); i++){//Handle the comments and adding spaces before semicolon
 		if(this->userInput[i] == '#'){
 			break;
-		} else{
-			if(this->userInput[i+1] == '' && (i+1) != this->userInput.size()){
-				
+		} else{ 
+			if(this->userInput[i+1] == " " && (i+1) != this->userInput.size()){
+				newUserInput += " ";
 			}
-			newUserString += this->userInput[i]; 
+			newUserInput += this->userInput[i]; 
 		}	
 	}
 	
 	
+	//Now our new user input string accounts for comments, and now everything is separated by a space
 	
 	
-	char *token = strtok(str," ");
+	//Now we build our tree as we tokenize
+	
+	char *token = strtok(newUserString," ");
     	while (token)
     	{
+		//Check size lists (command = 2 & connector = 1)
+		//Assign first command as connectors left child and second command as connectors right child
+		//CommandList should now be empty
+		//Push the connector onto the commandClass
+		
+		//Need to check if it is a command or connector
+		
+		//If command, then instantiate a command object, then push that command object onto commandList
+		//If connector, then instantiate a connector object and push that connector object onto connectorList
         	
         	token = strtok(NULL,delim);
 	}
