dc3bb704a4d4af2c9cd519d315cdc236a64f47e6
Nhat Nguyen <nnguy176@ucr.edu>
2019-02-17 19:20:29 -0800

    Updated Tests, Added loop to Main

--- integration_tests/commented_command_tests.sh
+++ integration_tests/commented_command_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INPUTS=('echo #b ' 'echo a || echo b && echo c #&& echo d && echo h' 'echo a && echo c &&  #echo')
+INPUTS=('echo #b ' 'echo a || echo b && echo c #&& echo d && echo h' '#echo a && echo b && echo c')
 
 .././integration ${INPUTS[2]}
 .././integration ${INPUTS[1]}
