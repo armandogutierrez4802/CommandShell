3700494bb879756794b898dea5236ff18af68782
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2019-03-15 20:45:06 -0700

    Update outputRed.cpp

--- src/outputRed.cpp
+++ src/outputRed.cpp
@@ -9,6 +9,8 @@ bool OutputRed::execute(int in, int out){
 	string outputFile = rightChild->getFileName();
 	out = open(outputFile.c_str(),O_WRONLY| O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
 
+	cout << "END OF OUTPUTREDEXECUTE" << endl;	
+	
 	return leftChild->execute(0,out);
 }
 
