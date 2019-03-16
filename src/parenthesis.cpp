f709e00f05adf3cc8dc6d6fc9e33f78a3c3ca5e3
Armando Gutierrez <armandogutierrez4802@gmail.com>
2019-03-15 20:33:11 -0700

    Need to trace the infinite loop

--- src/parenthesis.cpp
+++ src/parenthesis.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 Parenthesis::Parenthesis(CommandLine* onlyChild) {
 	this->leftChild = onlyChild;
+	this->rightChild = nullptr;
 }
         
 bool Parenthesis::execute(int in, int out){
-	//cout << "my () child is -> " << leftChild << endl;
-	return leftChild->execute(in,out);
+	cout << "my () child is -> " << leftChild << endl;
+	return leftChild->execute(0,1);
 }
 
 
--- src/pipe.cpp
+++ src/pipe.cpp
@@ -6,24 +6,35 @@ Pipe::Pipe(CommandLine* leftChild, CommandLine* rightChild) {
 }
         
 bool Pipe::execute(int in, int out){
+	cout << "Beginning of Pipe" << endl;
 	int fd[2];
 
 	if(pipe(fd) == -1){
 		perror("Error in pipe");
 		return false;
 	}
+	
+	cout << "PIPE A" << endl;
 
 	if(leftChild->execute(in,fd[1]) == false){
 		return false;
 	}
 
+	cout << "PIPE B" << endl;
+
 	close(fd[1]);
+
+	cout << "PIPE C" << endl;
 
 	if(rightChild->execute(fd[0],out) == false){
 		return false;
 	}
 
+	cout << "PIPE D" << endl;
+
 	close(fd[0]);
+	
+	cout << "End of pipe" << endl;
 
 	return true;
 }
