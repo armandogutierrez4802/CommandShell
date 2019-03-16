65ae77b9a05010ff620a43c7badf2715c44330e1
armandogutierrez4802 <armandogutierrez4802@gmail.com>
2019-03-15 20:52:04 -0700

    Update parenthesis.cpp

--- src/parenthesis.cpp
+++ src/parenthesis.cpp
@@ -7,16 +7,8 @@ Parenthesis::Parenthesis(CommandLine* onlyChild) {
         
 bool Parenthesis::execute(int in, int out){
 	cout << "my () child is -> " << leftChild << endl;
-	return leftChild->execute(0,1);
+	return leftChild->execute(in,out);
 }
 
-
-/*
-Notes on how to solve parenthesis:
-- Since we can have multiple parenthesis, have the flag be an int, rather than a bool, and have countParen == 0 be check for push onto "real/final" tree
-- Maybe when we come across the first parenthesis, we call a void function where we deal with all of the parenthesis in, and then return the final subtree
-	-> This subtree may contain multiple parenthesis or none.
-
-*/
 
 
